36746fe0c85d91655277f62d6a62690d941750ef
CassandraDeProspero <deprospero.2@osu.edu>
2020-02-25 08:44:07 -0500

    fix

--- src/books/Books.js
+++ src/books/Books.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Books.css";
 
-export default function Books() {
+//export default function Books() {
+render () {
     return (
         <div className="Books">
             <div className="lander">
-                <h1>Books</h1>
+                <BookDisplay books={this.state.books} />
             </div>
         </div>
     );
@@ -26,6 +27,42 @@ constructor(props) {
                 title: "Accelerate",
                 author: "Nicole Forsgren",
                 isbn: "978-1942788331"
+            },
+            {
+                id: 3,
+                title: "The Nickel Boys",
+                author: "Colson Whitehead",
+                isbn: "978-3861900365"
+            },
+            {
+                id: 4,
+                title: "Gingerbread",
+                author: "Helen Oyeyemi",
+                isbn: "978-2973309163"
+            },
+            {
+                id: 5,
+                title: "Nothing to See Here",
+                author: "Kevin Wilson",
+                isbn: "978-1739733202"
+            },
+            {
+                id: 6,
+                title: "The Old Drift",
+                author: "Namwali Serpell",
+                isbn: "978-1120973857"
+            },
+            {
+                id: 7,
+                title: "Women Talking",
+                author: "Miriam Toews",
+                isbn: "978-1128720979"
+            },
+            {
+                id: 8,
+                title: "The Dutch House",
+                author: "Ann Patchett",
+                isbn: "978-1000938710"
             }
         ]
     };
